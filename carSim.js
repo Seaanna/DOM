d249cc91a77dd811189cba721582e3450136ad5e
sea-anna <seaanna.thompson@gmail.com>
2016-08-08 12:21:31 -0700

    finished car sim

--- carSim.js
+++ carSim.js
@@ -1,54 +1,70 @@
 
-var x;
+var car;
 
 function createCar(make, model, year, color) {
-  // var speed = 0;
+  var speed = 0;
   return {
     carMaker: make,
     carModel: model,
     carYear: year,
     carColor: color,
-    // getSpeed: function() {return speed;},
-    // accelerate: function() {
-    //   if (speed >= 76) {
-    //       speed = 85;
-    //     } else {
-    //   speed = speed + 10;}
-    // },
-    // brake: function() {
-    //   if (speed <=7){
-    //     speed = 0;}
-    //   else {
-    //   speed = speed - 7;}
-    //   },
-    // failedBrake: function(){
-    //   var slow = getRandom(0, Math.floor(speed/2));
-    //   speed = speed - slow;
-    // },
+    getSpeed: function(){document.getElementById("outPut").innerHTML=speed;
+    },
+    accelerate: function(){
+        while(speed<85){
+            (speed = speed +1);
+            document.getElementById("outPut3").innerHTML+= " " +speed;
+        }      
+    },
+    brake: function(){
+        while(speed>0){
+            (speed = speed -1);
+            document.getElementById("outPut").innerHTML=speed;
+            //$('#output').html(speed);
+        } 
+    },
+    rollerCoaster: function(){
+        while(speed<70){
+            (speed = speed + 1);
+            document.getElementById("outPut").innerHTML=speed;
+        } 
+        while(speed>0){
+            (speed = speed -1);
+            document.getElementById("outPut").innerHTML=speed;
+        } 
+    },
+    breakDownHill: function(){
+        while(speed>0){
+            (speed=speed - (Math.floor((Math.random()*(speed/2))+1)));
+            document.getElementById("outPut2").innerHTML+= " " + speed;
+        }
+    },
     getData: function() {document.getElementById("outPut").innerHTML= this.carMaker+" "+ this.carModel+" "+this.carYear+" "+this.carColor;},
-    // accelerateTo50: function(){
-    //   while (speed < 50){
-    //     this.accelerate();
-    //   }
-    // },
-    // stop: function(){
-    //   while (speed > 6){
-    //     this.brake();
-    //   }
-    //   while (speed > 0){
-    //     speed--;
-    //   }
-    // }
   };
 }
 
 
 
+
 function getCarVars() {
-  x = createCar(document.getElementById("carMake").value,
+  car = createCar(document.getElementById("carMake").value,
   document.getElementById("carModel").value,
   document.getElementById("carYear").value,
   document.getElementById("carColor").value);
 
- (x.getData());
+  car.getData();
+}
+
+function goFast (){
+  car = createCar(document.getElementById("carMake").value,
+  document.getElementById("carModel").value,
+  document.getElementById("carYear").value,
+  document.getElementById("carColor").value);
+
+  car.accelerate();
+}
+
+
+function failBreak (){
+  car.breakDownHill();
 }
